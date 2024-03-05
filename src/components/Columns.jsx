a773ed93b052503cdba90bd8a0fd14bc6c58c7f1
Expert <dushyantkore@Dushyant-Kore.local>
2024-03-05 22:22:17 +0530

    removed workflow

--- src/components/Columns.jsx
+++ src/components/Columns.jsx
@@ -10,9 +10,9 @@ function Columns() {
         <div className='border-t-[1px] border-zinc-600'>
             <div>
                 {["Planetly","Workiz Easy","Premium Blend","Hypercare Systems","Officevibe","Orderlion","Black Book","Trawa Energy"].map((item,index)=> (
-                    <div key={index} className='flex justify-between px-14 text-[1.1vw] py-6 border-b-[1px] border-zinc-500'>
-                        <UnderlineAnime name={item}/>
-                        <h1>Nina Walloch</h1>
+                    <div key={index} className='flex justify-between px-14 text-[1.1vw] py-6 border-b-[1px] border-zinc-500 relative'>
+                        <div clas><UnderlineAnime name={item}/></div>
+                        <h1 className='absolute left-[45.7vw]'>Nina Walloch</h1>
                         <UnderlineAnime name="Read"/>
                     </div>
                 ))}
